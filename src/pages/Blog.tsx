dd6df471b43b10a9c02f063f0231cdd40f250b9d
MarzukhAsjad <marz.asjad00@gmail.com>
2025-10-02 11:34:26 +0800

    Modify image and content for first blog

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -5,7 +5,7 @@ import { TypingAnimation } from "@/components/magicui/terminal";
 const Blog: React.FC = () => {
   return (
     <div className="min-h-screen bg-purple-950 text-white">
-      <div className="container mx-auto px-4 py-8">
+      <div className="container mx-auto px-30 py-8">
         <header className="mb-8">
           <Link
             to="/"
@@ -39,12 +39,48 @@ const Blog: React.FC = () => {
               <img
                 src="/webhook_post_image.png"
                 alt="Webhooks"
-                className="rounded-2xl w-100 h-auto"
+                className="rounded-2xl w-150 h-auto"
               />
             </div>
             <caption className="flex justify-center text-gray-500 text-sm mb-4">
               Webhooks communication between microservices
             </caption>
+            <p className="text-gray-200 mb-4 text-justify">
+              When your company starts to grow, so does the complexity of your
+              systems and naturally, you will end up with having some
+              microservices. If you created one API, and are currently serving
+              less than 1000 users, and if you have some microservices for your
+              single programming language/framework API, then nah, microservices
+              are not worth it. But sometimes, even for a small user base, you
+              happen to have some microservices built in completely different
+              frameworks for different use cases. In my company for example, we
+              have one microservice that handles customer enquiry reports,
+              talking with third party services, and managing these reports,
+              let's call it the credit enquiry service. We also have another
+              microservice that is used internally by our operations team, i.e,
+              the loan management system. These two systems are built in two
+              different frameworks. The credit enquiry service is built with
+              Java Spring Boot, whereas the loan management system is built with
+              Node.js. I am not going to discuss the pros and cons of
+              microservices here, but one of the challenges you will face is how
+              to make these microservices communicate with each other.
+            </p>
+            <p className="text-gray-200 mb-4 text-justify">
+              With any large system, we have to take some assumptions.
+            </p>
+            <p className="text-gray-200 mb-1 text-justify">
+              1. These microservices are not allowed to directly access each
+              other's database.
+            </p>
+            <p className="text-gray-200 mb-1 text-justify">
+              2. They are REST API based.
+            </p>
+            <p className="text-gray-200 mb-1 text-justify">
+              3. Your distributed system does not handle excessively large
+              traffic, like in the scale of millions of requests per second
+              (even then it would technically be fine but requires further
+              optimisations).
+            </p>
             <div className="flex items-center text-sm text-gray-500">
               <span>Marzukh Akib Asjad</span>
               <span className="mx-2">•</span>
